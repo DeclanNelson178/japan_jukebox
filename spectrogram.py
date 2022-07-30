965017aa7a4bff3b3a772ae821858bba97126afa
declannelson <dfnelson178@gmail.com>
2022-07-30 10:28:54 -0400

    documentation and formatting

--- spectrogram.py
+++ spectrogram.py
@@ -1,12 +1,20 @@
 import argparse
 import curses
+import numpy as np
 import pickle
 import time
-from pathlib import Path
-
-import numpy as np
-
+from constants import (
+    DISPLAY_CHARS,
+    MIN_FREQ,
+    MAX_FREQ,
+    RED,
+    MAGENTA,
+    YELLOW,
+    GREEN,
+    CYAN,
+)
 from oscillating_line import oscillating_line
+from pathlib import Path
 from utils import (
     load_song,
     song_length,
@@ -18,33 +26,21 @@ from utils import (
     add_title,
 )
 
-### CLI ARGS ###
-parser = argparse.ArgumentParser(description="Trappin in Japan Jukebox")
-parser.add_argument("-v", "--volume", default=15)
-parser.add_argument("-t", "--type", default="fft")
-args = parser.parse_args([])
-
-VOLUME = args.volume
-TYPE = args.type
-
-MIN_FREQ, MAX_FREQ = 30, 200
-
-CHARS = ["s", "S", "$"]
-CHARS.reverse()
-
 
 def get_char(char_height, screen_height):
+    """Which character to display depending on distance from center line"""
     if char_height >= screen_height - 1 or char_height <= 1:
-        return CHARS[-1]
+        return DISPLAY_CHARS[-1]
 
     middle_screen = screen_height // 2
     dist = abs(middle_screen - char_height)
 
-    char_idx = int(dist / middle_screen * len(CHARS))
-    return CHARS[char_idx]
+    char_idx = int(dist / middle_screen * len(DISPLAY_CHARS))
+    return DISPLAY_CHARS[char_idx]
 
 
 def sfft(signal, window_rate, audio_length):
+    """Short Frame Fast Fourier Transform (no overlapping windows)"""
     num_frame_windows = window_rate * audio_length
     frames_list = np.array_split(signal, num_frame_windows)
     return [
@@ -55,12 +51,14 @@ def sfft(signal, window_rate, audio_length):
 
 
 def scale_amplitudes(frequencies, height):
+    """Convert amplitude signals to pixel heights"""
     max_freq = max(frequencies)
     max_freq = 1 if max_freq == 0 else max_freq
     return [int(height * (freq / max_freq)) for freq in frequencies]
 
 
 def spectrogram_data(signal, scr, audio_length, window_rate, audio_name):
+    """Convert .wav file to pixel height for varying frequency groups"""
     data_path = f"./cache/{audio_name}_sfft.bin"
     if Path(data_path).exists():
         return pickle.load(open(data_path, "rb"))
@@ -76,24 +74,24 @@ def spectrogram_data(signal, scr, audio_length, window_rate, audio_name):
         ]
         display_data.append(scale_amplitudes(consolidated_frequencies, height))
 
+    # save data in cache
     pickle.dump(display_data, open(data_path, "wb"))
 
     return display_data
 
 
 def color_picker(amplitude_pct):
+    """Color frequency group depending on amplitude"""
     if amplitude_pct > 0.6:
-        # red
-        return 1
+        return RED
     elif amplitude_pct > 0.5:
-        # magenta
-        return 2
+        return MAGENTA
     elif amplitude_pct > 0.4:
-        return 3
+        return YELLOW
     elif amplitude_pct > 0.2:
-        return 4
+        return GREEN
 
-    return 5
+    return CYAN
 
 
 def spectrogram_display(
@@ -106,15 +104,23 @@ def spectrogram_display(
     max_amplitude,
     window_rate,
 ):
+    """Show frequency groups amplitudes"""
     height, _ = scr.getmaxyx()
+
+    # start audio
     play(audio_name)
+
+    # keep track of time
     s = time.time()
     while (curr_time := time.time() - s) < audio_length:
+        # refresh screen
         scr.clear()
 
+        # get data idx depending on how far into song
         idx = int(curr_time * window_rate)
         idx = len(frequency_data) - 1 if idx >= len(frequency_data) else idx
 
+        # get frequency and amplitude, convert to display data
         frequencies = frequency_data[idx]
         frame_amplitude = amplitude_data[int(curr_time * frame_rate)]
         color = color_picker(frame_amplitude / max_amplitude)
@@ -122,10 +128,12 @@ def spectrogram_display(
         for x, amplitude in enumerate(frequencies):
             offset = (height - amplitude) // 2
             for i in range(0, amplitude + 1):
+                # curses is weird, make sure we don't display anything off page
                 y = height - 2 - i - offset
                 if y > height - 2:
                     y = height - 2
                 elif y < 0:
+                    # y = 0 and y = 1 both cause errors
                     y = 2
 
                 scr.addstr(
@@ -135,16 +143,20 @@ def spectrogram_display(
                     curses.color_pair(color) | curses.A_BOLD,
                 )
 
+        # show display
         scr.refresh()
+
+        # wait small amount of time
         scr.timeout(int(1 / window_rate * 1000))
+
+        # check for user interrupt
         if scr.getch() != -1:
             return True
     return False
 
 
 def spectrogram(audio_name):
-    if TYPE == "basic":
-        oscillating_line(audio_name)
+    # process ten sound-bites per second
     window_rate = 10
 
     song, signal = load_song(audio_name)
@@ -154,8 +166,10 @@ def spectrogram(audio_name):
     scr = set_up_curses()
     height, width = scr.getmaxyx()
 
+    # set up display with name of song
     title, pad = configure_display_screen(audio_name, height, width, buffer=3)
 
+    # perform fft on .wav file
     frequency_data = spectrogram_data(
         signal, pad, audio_length, window_rate, audio_name
     )
@@ -164,7 +178,6 @@ def spectrogram(audio_name):
     user_done = True
     try:
         add_title(title, scr)
-        s = time.time()
         user_done = spectrogram_display(
             pad,
             audio_name,
